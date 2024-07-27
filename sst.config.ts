c5ceeb63b57d2fd5b758f410a29b6aa7d32661cc
2underscores <jeremy__smith@hotmail.com>
2024-07-27 14:32:52 +1000

    Update sst config to include storage stack

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,5 @@
 import { SSTConfig } from "sst";
-import { API } from "./stacks/MyStack";
+import { StorageStack } from "./stacks/StorageStack";
 
 export default {
   config(_input) {
@@ -11,6 +11,6 @@ export default {
     };
   },
   stacks(app) {
-    app.stack(API);
-  }
+    app.stack(StorageStack); // key bit here
+  },
 } satisfies SSTConfig;
