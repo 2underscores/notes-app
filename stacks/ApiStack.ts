2a9bdbc72000c12a248a6c374a1fd18c71ca1d2a
2underscores <jeremy__smith@hotmail.com>
2024-07-27 14:32:53 +1000

    Added endpoints: Billing

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -1,13 +1,14 @@
-import { Api, StackContext, use } from "sst/constructs";
+import { Api, Config, StackContext, use } from "sst/constructs";
 import { StorageStack } from "./StorageStack";
 
 export function ApiStack({stack}: StackContext) {
     const {table} = use(StorageStack);
+    const STRIPE_SECRET_KEY = new Config.Secret(stack, "STRIPE_SECRET_KEY");
     const api = new Api(stack, 'Api', {
         defaults: {
             authorizer: 'iam',
             function: {
-                bind: [table], // TODO: Research bind. Just IAM?
+                bind: [table, STRIPE_SECRET_KEY], // TODO: Research bind. Just IAM? bind table, SSM param. #TODOx2.
             },
         },
         routes: {
@@ -16,6 +17,7 @@ export function ApiStack({stack}: StackContext) {
             "GET /notes/{id}": "packages/functions/src/get.main",
             "PUT /notes/{id}": "packages/functions/src/update.main",
             "DELETE /notes/{id}": "packages/functions/src/delete.main",
+            "POST /billing": "packages/functions/src/billing.main",
         },
     });
     // Print/show API in outputs
