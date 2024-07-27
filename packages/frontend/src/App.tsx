7ecf656d40d68448afb0d5152d9dc751fdd9c558
2underscores <jeremy__smith@hotmail.com>
2024-07-27 14:32:54 +1000

    Login/lougout with react state. No browser state

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,30 +3,48 @@ import { LinkContainer } from "react-router-bootstrap";
 // import Nav from "react-bootstrap/Nav"; // Can also import like this
 import Routes from "./Routes.tsx";
 import "./App.css";
+import { AppContext, AppContextType } from "./lib/contextLib";
+import { useState } from "react";
 
 
 //  Bootstrap spacing utility (like mb-# -margin bottom and py-# padding vertical (y))
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  function handleLogout() {
+    userHasAuthenticated(false);
+  }
+
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
         <LinkContainer to="/">
-          <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+          <Navbar.Brand className="fw-bold text-muted">Jez Notes</Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Nav activeKey={window.location.pathname}>
-            <LinkContainer to="/signup">
-              <Nav.Link>Signup</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <Nav.Link>Login</Nav.Link>
-            </LinkContainer>
+            {isAuthenticated ? (
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              ) : (
+                <>
+                  <LinkContainer to="/signup">
+                    <Nav.Link>Signup</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <Nav.Link>Login</Nav.Link>
+                  </LinkContainer>
+                </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <AppContext.Provider
+        value={{ isAuthenticated, userHasAuthenticated } as AppContextType}
+      >
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
 }
