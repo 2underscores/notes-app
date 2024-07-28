dee533226c156d0a86851042c3e93b7f2a7bba78
2underscores <jeremy__smith@hotmail.com>
2024-07-28 13:02:06 +1000

    Auth/unauth redirect away

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -4,14 +4,62 @@ import NotFound from "./containers/NotFound.tsx";
 import Login from "./containers/Login.tsx";
 import Signup from "./containers/Signup.tsx";
 import NewNote from "./containers/NewNote.tsx";
+import AuthenticatedRoute from "./components/AuthenticatedRoute.tsx";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute.tsx";
 
 export default function Links() {
+    // return (
+    //     <Routes>
+    //         <Route path="/" element={<Home />} />
+    //         <Route path="/login" element={<Login />} />
+    //         <Route path="/signup" element={<Signup />} />
+    //         <Route path="/notes/new" element={<NewNote/>} />
+    //         <Route path="*" element={<NotFound />} />;
+    //     </Routes>
+    // )
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/notes/new" element={<NewNote/>} />
+            <Route
+                path="/login"
+                element={
+                    <UnauthenticatedRoute>
+                        <Login />
+                    </UnauthenticatedRoute>
+                }
+            />
+            <Route
+                path="/signup"
+                element={
+                    <UnauthenticatedRoute>
+                        <Signup />
+                    </UnauthenticatedRoute>
+                }
+            />
+            {/* <Route
+                path="/settings"
+                element={
+                    <AuthenticatedRoute>
+                        <Settings />
+                    </AuthenticatedRoute>
+                }
+            /> */}
+            <Route
+                path="/notes/new"
+                element={
+                    <AuthenticatedRoute>
+                        <NewNote />
+                    </AuthenticatedRoute>
+                }
+            />
+            {/* <Route
+                path="/notes/:id"
+                element={
+                    <AuthenticatedRoute>
+                        <Notes />
+                    </AuthenticatedRoute>
+                }
+            /> */}
             <Route path="*" element={<NotFound />} />;
         </Routes>
     );
